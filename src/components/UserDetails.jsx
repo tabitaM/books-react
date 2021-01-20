6a8b68dba10d3c05787dba5edb1b6bed111e939a
Tabita <taby_marcu@yahoo.com>
2021-01-20 21:18:49 +0200

    Styling Home & Profile Page

--- src/components/UserDetails.jsx
+++ src/components/UserDetails.jsx
@@ -1,18 +1,49 @@
 import React from 'react'
 import useFetch from '../serices/useFetch'
+import styled from 'styled-components'
 
 export default function UserDetails() {
   const { data: userData } = useFetch('user')
   return (
     <div>
-      <img alt="" src={userData ? userData.avatar : ''} />
-      <div>
-        {userData ? userData.first_name + ' ' + userData.last_name : ''}
-      </div>
-      <div>{userData ? userData.role : ''}</div>
-      <div>City: {userData ? userData.city : ''}</div>
-      <div>Books: {userData ? userData.books_number : ''}</div>
-      <div>Return date: {userData ? userData.return_date : ''}</div>
+      <BasicUserDetails>
+        <Avatar alt="" src={userData ? userData.avatar : ''} />
+        <UserName>
+          {userData ? userData.first_name + ' ' + userData.last_name : ''}
+        </UserName>
+        <UserRole>{userData ? userData.role : ''}</UserRole>
+      </BasicUserDetails>
+      <hr style={{ border: '1px solid #8e7b6b', width: '95%' }}></hr>
+      <MoreUserDetails>
+        <div>City: {userData ? userData.city : ''}</div>
+        <div>Books: {userData ? userData.books_number : ''}</div>
+        <div>Return date: {userData ? userData.return_date : ''}</div>
+      </MoreUserDetails>
     </div>
   )
 }
+
+const Avatar = styled.img`
+  border-radius: 50%;
+  width: 250;
+  margin-bottom: 15px;
+`
+const BasicUserDetails = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 45px 0;
+`
+const UserName = styled.div`
+  font-size: 35px;
+  font-weight: bold;
+`
+
+const UserRole = styled.div`
+  font-size: 20px;
+`
+const MoreUserDetails = styled.div`
+  font-size: 18px;
+  margin: 20px 0 0 35px;
+  line-height: 2.2;
+`
--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -2,15 +2,43 @@ import React from 'react'
 import Button from '@material-ui/core/Button'
 import { useHistory } from 'react-router-dom'
 import useFetch from '../serices/useFetch'
+import styled from 'styled-components'
+import booksImage from '../assets/books.png'
 
 export default function Home() {
   const { data: userData } = useFetch('user')
   const navigate = useHistory()
 
   return (
-    <div>
-      Hello, {userData ? userData.first_name + ' ' + userData.last_name : ''}!
-      <Button onClick={() => navigate.push('/dashboard')}> check books </Button>
+    <div style={{ display: 'flex' }}>
+      <WelcomeMessage>
+        hello,{' '}
+        <UserName>
+          {userData ? userData.first_name + ' ' + userData.last_name : ''}!
+        </UserName>
+        <Button onClick={() => navigate.push('/dashboard')}>
+          {' '}
+          check books{' '}
+        </Button>
+      </WelcomeMessage>
+      <Image alt="" src={booksImage} />
     </div>
   )
 }
+
+const Image = styled.img`
+  width: 35%;
+  margin: 200px auto;
+`
+const WelcomeMessage = styled.h6`
+  margin-left: auto;
+  font-size: 30px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
+
+const UserName = styled.div`
+  font-size: 50px;
+`
